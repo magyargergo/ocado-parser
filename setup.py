d3cf7608b9948311efce70222ec678a595796d0b
SavageCore <talk@savagecore.uk>
2021-12-17 14:25:51 +0000

    correct readme filename

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     tests_require=[],
-    long_description=read('README.rst'),
+    long_description=read('README.md'),
 )
